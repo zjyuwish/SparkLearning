7ac3b9270f43e4d52239d4f0e6c1479ff7517c0e
Jinyu Zhang <zjyuwish@hotmail.com>
2017-09-26 16:05:10 +0800

    edit

--- src/main/java/CustomSortOne.scala
+++ src/main/java/CustomSortOne.scala
@@ -6,6 +6,21 @@ object CustomSortOne {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("CustomSortOne")
     val sc = new SparkContext(conf)
-    val rdd1 = sc.parallelize(List(("Yufeihong", 90, 28, 1), ("angelababy", 90, 27, 2)))
+    val rdd1 = sc.parallelize(List(("Yufeihong", 90, 28, 1), ("angelababy", 90, 27, 2), ("JuJingYi", 95, 22, 3)))
+    val rdd2 = rdd1.sortBy(x => Girl(x._2, x._3), false)
+    println(rdd2.collect().toBuffer)
+    sc.stop()
+  }
+
+
+}
+case class Girl(val faceValue: Int, val age: Int) extends Ordered[Girl] with Serializable {
+  override def compare(that: Girl): Int = {
+    if(this.faceValue == that.faceValue) {
+      this.age - that.age
+    }
+    else {
+      this.faceValue - that.faceValue
+    }
   }
 }
---
+++ src/main/java/CustomSortTwo.scala
@@ -0,0 +1,28 @@
+/**
+  * Created by jinyuzhang on 9/26/17.
+  */
+import org.apache.spark.{SparkConf, SparkContext}
+//object OrderContext {
+//  implicit val girlOrdering  = new Ordering[Girl] {
+//    override def compare(x: Girl, y: Girl): Int = {
+//      if(x.faceValue > y.faceValue) 1
+//      else if (x.faceValue == y.faceValue) {
+//        if(x.age > y.age) -1 else 1
+//      } else -1
+//    }
+//  }
+//}
+//object CustomSortTwo {
+//  def main(args: Array[String]): Unit = {
+//    val conf = new SparkConf().setAppName("CustomSortOne")
+//    val sc = new SparkContext(conf)
+//    val rdd1 = sc.parallelize(List(("Yufeihong", 90, 28, 1), ("angelababy", 90, 27, 2), ("JuJingYi", 95, 22, 3)))
+//    import OrderContext._
+//    val rdd2 = rdd1.sortBy(x => Girl(x._2, x._3), false)
+//    println(rdd2.collect().toBuffer)
+//    sc.stop()
+//  }
+//
+//
+//}
+//case class Girl(val faceValue: Int, val age: Int) extends serializable

a29ac9f0d9d5bb6f33b14c969e0a334840dac922
Jinyu Zhang <zjyuwish@hotmail.com>
2017-10-26 09:58:12 +0800

    edit

--- src/main/java/kafkaWordCount.scala
+++ src/main/java/kafkaWordCount.scala
@@ -3,8 +3,9 @@
   */
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.{HashPartitioner, SparkConf}
-import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.kafka.KafkaUtils
+
 
 object kafkaWordCount {
   val updateFunc = (iter: Iterator[(String, Seq[Int], Option[Int])]) => {
@@ -22,6 +23,7 @@ object kafkaWordCount {
     val data = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap, StorageLevel.MEMORY_AND_DISK_SER)
     val words = data.map(_._2).flatMap(_.split(" "))
     val wordCounts = words.map((_, 1)).updateStateByKey(updateFunc, new HashPartitioner(ssc.sparkContext.defaultParallelism), true)
+    wordCounts.print()
     ssc.start()
     ssc.awaitTermination()
   }
